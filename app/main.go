df6c020012bb9bcc430a8965160c5c8167c6efa8
Antonin Lenfant-Kodia <antoninweb@gmail.com>
2023-02-08 23:42:58 +0100

    create chroot jail

--- app/main.go
+++ app/main.go
@@ -2,20 +2,52 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
 	"os/exec"
+	"path/filepath"
+	"syscall"
 )
 
 // Usage: your_docker.sh run <image> <command> <arg1> <arg2> ...
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	command := os.Args[3]
 	args := os.Args[4:len(os.Args)]
 
+	// Create temporary directory to chroot in
+	chrootDir, err := ioutil.TempDir("", "")
+	if err != nil {
+		panic(err)
+	}
+	defer os.RemoveAll(chrootDir)
+
+	// Copy binary to run (no symlink possible)
+	targetPath := filepath.Join(chrootDir, command)
+	err = os.MkdirAll(filepath.Dir(targetPath), os.ModePerm)
+	if err != nil {
+		panic(err)
+	}
+	bytesRead, err := ioutil.ReadFile(command)
+	if err != nil {
+		panic(err)
+	}
+	err = ioutil.WriteFile(targetPath, bytesRead, os.ModePerm)
+	if err != nil {
+		panic(err)
+	}
+
+	// Run chroot
+	err = syscall.Chroot(chrootDir)
+	if err != nil {
+		panic(err)
+	}
+
+	// Run the command
 	cmd := exec.Command(command, args...)
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
-	err := cmd.Run()
+	cmd.Stdin = os.Stdin
+	err = cmd.Run()
 	if err != nil {
 		fmt.Printf("Err: %v", err)
 		if ee, ok := err.(*exec.ExitError); ok {
